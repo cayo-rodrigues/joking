77ab999b120c66e2242c4c23e3b3a252ad949760
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-07-26 10:29:43 -0300

    refact: use session permanent app config

--- app/app.py
+++ app/app.py
@@ -24,7 +24,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
